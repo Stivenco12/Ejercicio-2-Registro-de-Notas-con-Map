4c61a63c29dd60592553f12acfd4792e745d5ecf
jedier stivenson correa amaris <153353269+Stivenco12@users.noreply.github.com>
2025-02-23 16:42:21 -0500

    terminado
    se agregaron los ultimos 3 puntos pedidos

--- src/main/java/com/ejercicio2/Main.java
+++ src/main/java/com/ejercicio2/Main.java
@@ -25,7 +25,30 @@ public class Main {
                 for (Map.Entry<String, Double> entry : estudiantes.entrySet()) {
                     System.out.println("la nota de "+entry.getKey() + " es = " + entry.getValue());
                 }
+                System.out.println();
+                System.out.print("Quieres buscar un nombre? \n1. si\n2. no \nElije =  ");
+                int buscar = sc.nextInt();
+                sc.nextLine();
+                if (buscar == 1) {
+                    System.out.print("Ingrese el nombre que quiere buscar = ");
+                    String nombreBuscado = sc.nextLine();
                 
+                    if (estudiantes.containsKey(nombreBuscado)) { 
+                        System.out.println("El nombre que buscas sí existe");
+                    } else {
+                        System.out.println("El nombre que buscas no existe");
+                    }
+                }
+                String mejorEstudiante = "";
+                double notaMasAlta = Double.MIN_VALUE;
+                for (Map.Entry<String, Double> entry : estudiantes.entrySet()) {
+                    if (entry.getValue() > notaMasAlta) {
+                        notaMasAlta = entry.getValue();
+                        mejorEstudiante = entry.getKey();
+                    }
+                }
+                System.out.println("El estudiante con mejor rendimiento es " + mejorEstudiante + " con una nota de " + notaMasAlta);
+                System.out.println();
             }while(!menu.equals("2"));
         }catch (Exception e) {
             System.out.println("error por favor vuelve a ejecutrar el programa");
